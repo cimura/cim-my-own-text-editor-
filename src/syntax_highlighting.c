7456f78362634dbb2266be8e915986bfdc8d2011
Your Name <you@example.com>
2024-10-21 10:37:18 +0900

    add include highlight

--- src/syntax_highlighting.c
+++ src/syntax_highlighting.c
@@ -6,7 +6,7 @@
 char  *C_HL_extensions[] = {".c", ".h", ".cpp", NULL};
 char  *C_HL_keywords[] = {
     "switch", "if", "while", "for", "break", "continue", "return", "else",
-    "struct", "union", "typedef", "static", "enum", "class", "case",
+    "struct", "union", "typedef", "static", "enum", "class", "case", "include",
 
     "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
     "void|", NULL
